4fcc0bf9217e27b65171335d094c138bc5ebf06a
Chris Yang <cyang@deltalearns.ca>
2023-05-28 18:07:45 -0700

    update

--- example1.py
+++ example1.py
@@ -3,6 +3,8 @@
 # A basic canvas drawing using some geometric shapes
 
 import tkinter as tk
+import random
+import time
 
 w = tk.Tk()
 w.geometry("600x400")
@@ -12,8 +14,27 @@ c = tk.Canvas(width=550,height=450,background="#cccccc",bd="2")
 c.pack()
 
 
-rec = c.create_rectangle(50,50,80,80,fill="#aa0000")
-cir = c.create_oval(100,120,150,170,fill="green")
+def randomColor():
+    #converts rgb values to hex color code
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)    
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+# create 10 random objects on the canvas
+# create an empty list
+# iterate 10 times to populate list
+#   create random coordinates
+#   add a rectangle to the list of objects by appending it to the list
+objects = []
+colors = ['blue','green','red','yellow','white','black','#888888',"#ffff77"]
+for i in range(10):
+    x1 = random.randint(0,500)
+    y1 = random.randint(0,400)
+    x2 = x1 + random.randint(20,40)
+    y2 = y1 + random.randint(20,40)
+    objects.append( c.create_rectangle(x1,y1,x2,y2,fill=randomColor() ) )
+
 
 
 w.mainloop()
--- example2.py
+++ example2.py
@@ -3,6 +3,8 @@
 # A basic canvas drawing using some geometric shapes
 
 import tkinter as tk
+import random
+import time
 
 w = tk.Tk()
 w.geometry("600x400")
@@ -12,37 +14,17 @@ c = tk.Canvas(width=550,height=450,background="#cccccc",bd="2")
 c.pack()
 
 
-rec = c.create_rectangle(50,50,80,80,fill="#aa0000")
+#Placing blocks by coordinates assuming all blacks are the same size and 20x20 pixels
+map = [ (0,1) , (3,4) , (5,10) , (9,0) , (10,3)]
+walls = []
+for i in map:
+    x1 = i[0]*20 + 5
+    y1 = i[1]*20 + 5
+    x2 = x1+20
+    y2 = y1+20
+    walls.append( c.create_rectangle(x1,y1,x2,y2,fill="#aa00aa"))
 
 
-def leftKey(e):
-    print(e)
-    x=-5
-    y=0
-    c.move(rec,x,y)
-
-def rightKey(e):
-    print(e)
-    x=5
-    y=0
-    c.move(rec,x,y)
-
-def upKey(e):
-    print(e)
-    x=0
-    y=-5
-    c.move(rec,x,y)
-
-def downKey(e):
-    print(e)
-    x=0
-    y=5
-    c.move(rec,x,y)
-    
-w.bind("<Left>",leftKey)
-w.bind("<Right>",rightKey)
-w.bind("<Up>",upKey)
-w.bind("<Down>",downKey)
 
 
 w.mainloop()
