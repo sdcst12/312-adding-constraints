ab5bc5ee06f1a2483bc98f663c4a5ac4af8b5c76
Chris Yang <cyang@deltalearns.ca>
2023-05-30 07:54:26 -0700

    update examples

--- example3.py
+++ example3.py
@@ -1,12 +1,9 @@
 #!python3
 
-# A basic canvas drawing using some geometric shapes
+# Adding an image instead of using canvas basic shapes
 
 import tkinter as tk
-from PIL import ImageTk,Image
 import random
-import time
-
 w = tk.Tk()
 w.geometry("600x400")
 w.title("sample")
@@ -15,25 +12,26 @@ c = tk.Canvas(width=550,height=450,background="#cccccc",bd="2")
 c.pack()
 
 
-#Placing blocks by coordinates assuming all blacks are the same size and 20x20 pixels
-def getImage(x,y):
+r = c.create_rectangle(190,190,210,210,fill="#0000ff")
 
-    img = Image.open("assets/tiles.png").convert("RGBA")
-    xi = x*64
-    yi = y*64
-    img2 = img.crop([xi,yi,xi+64,yi+64])
-    return ImageTk.PhotoImage(img2)
+# Creates an image and places it with its center at 200,200
+wimg = tk.PhotoImage(file="assets/charmander.png")
+img = c.create_image(200,200,image=wimg)
 
+# Note that the rectangle r is behind the image.  You can bring it in front
+# untag the line 23 command
+#c.tag_raise(r)
 
-#img = tk.PhotoImage(file="assets/tiles.png")
-map = [ (0,1) , (3,4) , (5,10) , (9,0) , (10,3)]
-walls = []
-img = [[]]
-for i in range(4):
-    for j in range(4):
-        print(i,j)
-        img.append(getImage(i,j+8))
-        walls.append(c.create_image(i*64+32,j*64+32,image=img[-1]))
+# We can also add walls using images and add them to a list of walls
+walls  =[]
+wallImg = tk.PhotoImage(file="assets/win.png")
+for i in range(30):
+    x = random.randint(0,25)*20
+    y = random.randint(0,20)*20
+    walls.append( c.create_image(x,y,image=wallImg))
+
+c.tag_raise(img)
+
 
 
 
